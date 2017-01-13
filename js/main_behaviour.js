04f7e4cc2d2810dd5add31fb8ad2a4819a9f29f5
Gabriel <cladangab@gmail.com>
2017-01-13 00:10:49 -0200

    Update 4 - Menu/NavBar
    Positions and responsive adjustes.

---
+++ js/main_behaviour.js
@@ -0,0 +1,9 @@
+
+//element.parentNode.removeChild(menuMobile);
+//console.log("hOI!");
+
+window.onload = function () {
+    var menuMobile = document.getElementById("NavBar");
+    //menuMobile.parentNode.removeChild(menuMobile);
+    console.log(menuMobile);
+};
