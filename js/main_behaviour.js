7f3edeff341002084cee76375784bca939dee4b2
Gabriel <cladangab@gmail.com>
2017-01-14 21:25:07 -0200

    Update 5 - Menu/NavBar
    - Menu button functional in latest Chrome version;
    - NavBar animations functional in latest Chrome version;

--- js/main_behaviour.js
+++ js/main_behaviour.js
@@ -2,8 +2,43 @@
 //element.parentNode.removeChild(menuMobile);
 //console.log("hOI!");
 
+var menuMobile;
+
+var menuButtonPosition;
+
 window.onload = function () {
-    var menuMobile = document.getElementById("NavBar");
-    //menuMobile.parentNode.removeChild(menuMobile);
-    console.log(menuMobile);
+    menuMobile = document.getElementById("NavBar");
+    
+    menuMobile.style.transition = "none";
+    
+    menuButtonPosition = menuMobile.style.right;
+    
 };
+
+function MenuButtonClick(){
+    menuButtonPosition = menuMobile.style.right
+    
+    menuMobile.style.transition = "right 1s";
+    
+    if(window.innerWidth < 600){
+        if(menuButtonPosition == "100%"){
+            menuMobile.style.right = "0%";
+        }else if(menuButtonPosition == "0%"){
+            menuMobile.style.right = "100%";
+        }else{
+            menuButtonPosition = menuMobile.style.right = "0%";
+        }
+    }else{
+        menuButtonPosition = menuMobile.style.right = "0%";
+    }
+}
+
+function resize(){
+    if(window.innerWidth < 600){
+        menuMobile.style.transition = "none";
+        menuButtonPosition = menuMobile.style.right = "100%";
+    }else{
+        menuMobile.style.transition = "none";
+        menuButtonPosition = menuMobile.style.right = "0%";
+    }
+}
